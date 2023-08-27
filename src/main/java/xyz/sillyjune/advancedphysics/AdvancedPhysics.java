5f0a649a49367b2a34bf5320ef2808555ed33e03
Juniper <86971433+JuneDeveloper@users.noreply.github.com>
2023-08-27 14:38:36 +0100

    Initial Commit

---
+++ src/main/java/xyz/sillyjune/advancedphysics/AdvancedPhysics.java
@@ -0,0 +1,13 @@
+package xyz.sillyjune.advancedphysics;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AdvancedPhysics implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/xyz/sillyjune/advancedphysics/client/AdvancedPhysicsClient.java
@@ -0,0 +1,11 @@
+package xyz.sillyjune.advancedphysics.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdvancedPhysicsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
